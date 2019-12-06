332b7082eb052c670c5dd623d0385ea5ac16a78c
Xray3D <xray3d@ya.ru>
2019-12-06 17:21:40 +0300

    6/12/19

--- gcode/gcvoronoi.cpp
+++ gcode/gcvoronoi.cpp
@@ -2,14 +2,19 @@
 #include "gcfile.h"
 #include "gcvoronoi.h"
 
+#define JCV
+
 #ifdef JCV
 #include "voroni/jc_voronoi.h"
 #endif
 
+#ifdef SDG2_
+#include <CGAL/Algebraic_structure_traits.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Segment_Delaunay_graph_2.h>
 #include <CGAL/Segment_Delaunay_graph_filtered_traits_2.h>
 #include <CGAL/Segment_Delaunay_graph_storage_traits_with_info_2.h>
+#include <CGAL/Segment_Delaunay_graph_traits_2.h>
 
 // an enum representing the color
 enum /*Red_blue*/ {
@@ -57,98 +62,29 @@ struct Red_blue_merge_info {
     }
 };
 
+//typedef CGAL::Simple_cartesian<double> K;
 using K = CGAL::Exact_predicates_inexact_constructions_kernel;
 using Gt = CGAL::Segment_Delaunay_graph_filtered_traits_2<K>;
-// define the storage traits with info
 using ST = CGAL::Segment_Delaunay_graph_storage_traits_with_info_2<Gt, Red_blue, Red_blue_convert_info, Red_blue_merge_info>;
-using SDG2 = CGAL::Segment_Delaunay_graph_2<Gt, ST>;
+using D_S = CGAL::Triangulation_data_structure_2<CGAL::Segment_Delaunay_graph_vertex_base_2<ST>, CGAL::Segment_Delaunay_graph_face_base_2<Gt>>;
+using SDG2 = CGAL::Segment_Delaunay_graph_2<Gt, ST, D_S>;
 
-//// includes for defining the Voronoi diagram adaptor
-//#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-//#include <CGAL/Segment_2.h>
-//#include <CGAL/Segment_Delaunay_graph_2.h>
-//#include <CGAL/Segment_Delaunay_graph_adaptation_policies_2.h>
-//#include <CGAL/Segment_Delaunay_graph_adaptation_traits_2.h>
-//#include <CGAL/Segment_Delaunay_graph_storage_traits_with_info_2.h>
-//#include <CGAL/Segment_Delaunay_graph_traits_2.h>
-//#include <CGAL/Voronoi_diagram_2.h>
+inline IntPoint toIntPoint(const CGAL::Point_2<K>& point)
+{
+    return IntPoint{ static_cast<cInt>(point.x()), static_cast<cInt>(point.y()) };
+}
 
-//// an enum representing the color
-//enum Red_blue {
-//    RED = 1,
-//    BLUE = 2,
-//    PURPLE = 3
-//};
-
-//// functor that defines how to convert color info when:
-//// 1. constructing the storage site of an endpoint of a segment
-//// 2. a segment site is split into two sub-segments
-//struct Red_blue_convert_info {
-//    typedef Red_blue Info;
-//    typedef const Info& result_type;
-
-//    inline const Info& operator()(const Info& info0, bool) const
-//    {
-//        // just return the info of the supporting segment
-//        return info0;
-//    }
-
-//    inline const Info& operator()(const Info& info0, const Info&, bool) const
-//    {
-//        // just return the info of the supporting segment
-//        return info0;
-//    }
-//};
-
-//// functor that defines how to merge color info when a site (either
-//// point or segment) corresponds to point(s) on plane belonging to
-//// more than one input site
-//struct Red_blue_merge_info {
-//    typedef Red_blue Info;
-//    typedef Info result_type;
-
-//    inline Info operator()(const Info& info0, const Info& info1) const
-//    {
-//        // if the two sites defining the new site have the same info, keep
-//        // this common info
-//        if (info0 == info1) {
-//            return info0;
-//        }
-//        // otherwise the new site should be purple
-//        return PURPLE;
-//    }
-//};
-
-//using K = CGAL::Exact_predicates_inexact_constructions_kernel;
-//using Gt = CGAL::Segment_Delaunay_graph_traits_2<K>;
-
-//using ST = CGAL::Segment_Delaunay_graph_storage_traits_with_info_2<Gt,
-//    Red_blue,
-//    Red_blue_convert_info,
-//    Red_blue_merge_info>;
-
-//using DT = CGAL::Segment_Delaunay_graph_2<Gt, ST>;
-//using AT = CGAL::Segment_Delaunay_graph_adaptation_traits_2<DT>;
-//using AP = CGAL::Segment_Delaunay_graph_degeneracy_removal_policy_2<DT>;
-//using VD = CGAL::Voronoi_diagram_2<DT, AT, AP>;
-//using Site_2 = AT::Site_2;
-//using Face_handle = VD::Face_handle;
-
-//inline bool qMapLessThanKey(const IntPoint& key1, const IntPoint& key2)
-//{
-//    return qHash(QByteArray(reinterpret_cast<const char*>(&key1), sizeof(IntPoint))) < qHash(QByteArray(reinterpret_cast<const char*>(&key2), sizeof(IntPoint)));
-//}
-//inline bool qMapLessThanKey(const Site_2& key1, const Site_2& key2)
-//{
-//    return qHash(QByteArray(reinterpret_cast<const char*>(&key1), sizeof(Site_2))) < qHash(QByteArray(reinterpret_cast<const char*>(&key2), sizeof(Site_2)));
-//}
-
+inline uint qHash(const IntPoint& key, uint /*seed*/ = 0)
+{
+    return qHash(QByteArray(reinterpret_cast<const char*>(&key), sizeof(IntPoint)));
+}
+#endif
 namespace GCode {
 
 #ifdef JCV
 inline uint qHash(const GCode::VoronoiCreator::Pair& tag, uint seed = 0)
 {
-    return ::qHash(tag.first.X * tag.second.X, seed ^ 0xa317a317) ^ ::qHash(tag.first.Y * tag.second.Y, seed ^ 0x17a317a3);
+    return ::qHash(tag.first.X ^ tag.second.X /*, seed ^ 0xa317a317*/) ^ ::qHash(tag.first.Y ^ tag.second.Y /*, seed ^ 0x17a317a3*/);
 }
 #endif
 
@@ -164,79 +100,50 @@ void VoronoiCreator::create(const GCodeParams& gcp)
 void VoronoiCreator::createVoronoi(const Tool& tool, double depth, const double tolerance, const double width)
 {
     self = this;
-
-    CleanPolygons(m_workingPs, tolerance * 0.1 * uScale);
+    CleanPolygons(m_workingPs, /*tolerance **/ 0.001 * uScale);
     groupedPaths(CopperPaths);
 
-    //    VD vd;
+#ifdef SDG2_
+    cInt minX = std::numeric_limits<cInt>::max(),
+         minY = minX,
+         maxX = std::numeric_limits<cInt>::min(),
+         maxY = maxX;
 
-    //    int id = 0;
-
-    //    cInt minX = std::numeric_limits<cInt>::max(), minY = minX, maxX = std::numeric_limits<cInt>::min(), maxY = maxX;
-
-    //    //    QMap<IntPoint, int> map;
-    //    QMap<Site_2, int> map;
-    //    for (const Paths& paths : m_groupedPss) {
-    //        for (const Path& path : paths) {
-    //            for (int i = 0; i < path.size(); ++i) {
-    //                const IntPoint& point = path[i];
-    //                //                map[point] = id;
-
-    //                Site_2 site;
-    //                site = !i ? CGAL::Segment_Delaunay_graph_site_2<K>::construct_site_2({ path.last().X, path.last().Y }, { point.X, point.Y }, id)
-    //                          : CGAL::Segment_Delaunay_graph_site_2<K>::construct_site_2({ path[i - 1].X, path[i - 1].Y }, { point.X, point.Y }, id);
-    //                vd.insert(site);
-    //                map[site] = id;
-    //                maxX = std::max(maxX, point.X);
-    //                maxY = std::max(maxY, point.Y);
-    //                minX = std::min(minX, point.X);
-    //                minY = std::min(minY, point.Y);
-    //            }
-    //            ++id;
-    //        }
-    //    }
-    //    Paths line;
-    //    assert(vd.is_valid());
-    //    qDebug() << vd.number_of_faces();
-    //    VD::Face_iterator it = vd.faces_begin(), beyond = vd.faces_end();
-    //    for (int f = 0; it != beyond; ++f, ++it) {
-    //        qDebug() << "Face" << f << ":";
-    //        VD::Ccb_halfedge_circulator hec = it->ccb();
-    //        Path path;
-    //        do {
-    //            VD::Halfedge_handle heh = static_cast<VD::Halfedge_handle>(hec);
-    //            if (heh->has_target()) {
-    //                if (!heh->has_source() || !heh->has_target()) {
-    //                    qDebug("continue 1");
-    //                    continue;
-    //                }
-    //                //                if (map[heh->left()->face()->vertex()] == map[heh->right()->site()]) {
-    //                //                    qDebug() << "continue 2" << map[heh->left()->site()] << map[heh->right()->site()];
-    //                //                    continue;
-    //                //                }
-    //                // qDebug() <<heh->left()->face();
-    //                path.append(IntPoint {
-    //                    static_cast<cInt>(heh->target()->point().x()),
-    //                    static_cast<cInt>(heh->target()->point().y()) });
-    //            } else {
-    //                qDebug("point at infinity");
-    //            }
-    //        } while (++hec != it->ccb());
-    //        line.append(path);
-    //    }
-
+    auto generateFile = [this, tool, depth](const QString& name, Paths& paths) {
+        m_file = new File({ paths }, tool, depth, Voronoi, m_workingRawPs);
+        m_file->setFileName(name);
+        emit fileReady(m_file);
+    };
+    auto clip = [&maxX, &maxY, &minX, &minY, this](Paths& paths) {
+        Clipper clipper;
+        clipper.AddPaths(paths, ptSubject, false);
+        clipper.AddPath({ { minX - uScale, minY - uScale },
+                            { minX - uScale, maxY + uScale },
+                            { maxX + uScale, maxY + uScale },
+                            { maxX + uScale, minY - uScale } },
+            ptClip, true);
+        clipper.Execute(ctIntersection, paths, pftNonZero);
+        Clipper clipper2;
+        clipper2.AddPaths(paths, ptSubject, false);
+        for (const Paths& paths : m_groupedPss)
+            clipper2.AddPaths(paths, ptClip, true);
+        clipper2.Execute(ctDifference, paths, pftNonZero);
+    };
+    progress(2, 0);
     SDG2 sdg;
-
-    cInt minX = std::numeric_limits<cInt>::max(), minY = minX, maxX = std::numeric_limits<cInt>::min(), maxY = maxX;
+    QSet<IntPoint> testSet;
     //add line segments to diagram
     int id = 0;
     for (const Paths& paths : m_groupedPss) {
         for (const Path& path : paths) {
             for (int i = 0; i < path.size(); ++i) {
                 const IntPoint& point = path[i];
+                testSet.insert(point);
                 SDG2::Site_2 site;
-                site = !i ? CGAL::Segment_Delaunay_graph_site_2<K>::construct_site_2({ path.last().X, path.last().Y }, { point.X, point.Y }, id)
-                          : CGAL::Segment_Delaunay_graph_site_2<K>::construct_site_2({ path[i - 1].X, path[i - 1].Y }, { point.X, point.Y }, id);
+                site = !i ? CGAL::Segment_Delaunay_graph_site_2<K>::
+                                construct_site_2({ path.last().X, path.last().Y }, { point.X, point.Y })
+                          : CGAL::Segment_Delaunay_graph_site_2<K>::
+                                construct_site_2({ path[i - 1].X, path[i - 1].Y }, { point.X, point.Y });
                 sdg.insert(site, id);
                 maxX = std::max(maxX, point.X);
                 maxY = std::max(maxY, point.Y);
@@ -246,83 +153,112 @@ void VoronoiCreator::createVoronoi(const Tool& tool, double depth, const double 
             ++id;
         }
     }
+
     assert(sdg.is_valid(true, 1));
-    //    std::vector<SDG2::Site_2> sites;
-    //    sdg.insert(sites.begin(), sites.end(), CGAL::Tag_true());
 
-    //save voronoi edges to a file
-    //    sdg.draw_dual(ofs);
+    Paths segments;
 
-    Paths line;
-    Paths segment;
-    Paths ray;
-    Paths parabolaSegment;
-
-    //    using FVIT = SDG2::Finite_vertices_iterator;
-    //    for (FVIT it = sdg.finite_vertices_begin(); it != sdg.finite_vertices_end(); ++it) {
-    //        SDG2::Site_2 site = it->site();
-    //        line std::cout << "\t" << it->storage_site().info();
-    //    }
-
-    SDG2::Finite_edges_iterator eit = sdg.finite_edges_begin();
-    for (; eit != sdg.finite_edges_end(); ++eit) {
+    for (SDG2::Finite_edges_iterator eit = sdg.finite_edges_begin(); eit != sdg.finite_edges_end(); ++eit) {
         const SDG2::Edge e = *eit;
         CGAL_precondition(!sdg.is_infinite(e));
-        typename SDG2::Geom_traits::Line_2 l;
-        typename SDG2::Geom_traits::Segment_2 s;
-        typename SDG2::Geom_traits::Ray_2 r;
 
-        using LocalGt = CGAL::Segment_Delaunay_graph_filtered_traits_2<K>;
-        CGAL::Parabola_segment_2<LocalGt> ps;
+        // get the vertices defining the Voronoi edge
+        SDG2::Geom_traits::Line_2 sdgLine;
+        SDG2::Geom_traits::Segment_2 sdgSegment;
+        SDG2::Geom_traits::Ray_2 sdgRay;
+        CGAL::Parabola_segment_2<Gt> cgalParabola;
 
         CGAL::Object o = sdg.primal(e);
 
-        if (CGAL::assign(l, o)) {
-            //qDebug("Line");
-            line.append({
-                IntPoint { static_cast<cInt>(l.point(0).x()), static_cast<cInt>(l.point(0).y()) },
-                IntPoint { static_cast<cInt>(l.point(1).x()), static_cast<cInt>(l.point(1).y()) },
-            });
-        }
-        if (CGAL::assign(s, o) && !s.is_degenerate()) {
-            //qDebug("Segment");
-            segment.append({
-                IntPoint { static_cast<cInt>(s.point(0).x()), static_cast<cInt>(s.point(0).y()) },
-                IntPoint { static_cast<cInt>(s.point(1).x()), static_cast<cInt>(s.point(1).y()) },
-            });
-        }
-        if (CGAL::assign(r, o)) {
-            //qDebug("Ray");
-            ray.append({
-                IntPoint { static_cast<cInt>(r.point(0).x()), static_cast<cInt>(r.point(0).y()) },
-                IntPoint { static_cast<cInt>(r.point(1).x()), static_cast<cInt>(r.point(1).y()) },
-            });
-        }
-        if (CGAL::assign(ps, o)) {
-            //qDebug("Parabola_segment");
+        if (CGAL::assign(sdgLine, o)) {
+            Path path{ toIntPoint(sdgLine.point(0)), toIntPoint(sdgLine.point(1)) };
+            if (!testSet.contains(path.first()) && !testSet.contains(path.last()))
+                segments /*line*/.append(path);
+        } else if (CGAL::assign(sdgRay, o)) {
+            Path path{ toIntPoint(sdgRay.point(0)), toIntPoint(sdgRay.point(1)) };
+            if (!testSet.contains(path.first()) && !testSet.contains(path.last()))
+                segments /*ray*/.append(path);
+        } else if (CGAL::assign(sdgSegment, o) && !sdgSegment.is_degenerate()) {
+            Path path{ toIntPoint(sdgSegment.point(0)), toIntPoint(sdgSegment.point(1)) };
+            if (!testSet.contains(path.first()) && !testSet.contains(path.last()))
+                segments.append(path);
+        } else if (CGAL::assign(cgalParabola, o)) {
             std::vector<CGAL::Point_2<K>> points;
-            ps.generate_points(points);
-            qDebug() << "points" << points.size();
+            cgalParabola.generate_points(points);
+
             Path path;
             path.reserve(static_cast<int>(points.size()));
-            for (const CGAL::Point_2<K>& pt : points) {
-                path << IntPoint { static_cast<cInt>(pt.x()), static_cast<cInt>(pt.y()) };
-            }
-            clean(path);
-            parabolaSegment.append(path);
+            for (const CGAL::Point_2<K>& pt : points)
+                path << toIntPoint(pt);
+            //            clean(path);
+            segments.append(path);
         }
     }
+    clip(segments);
 
-    auto generateFile = [this, tool, depth](const QString& name, Paths& paths) {
-        m_file = new File({ paths }, tool, depth, Voronoi, m_workingRawPs);
-        m_file->setFileName(name);
-        emit fileReady(m_file);
-    };
+    //sortBE(segments);
 
-    generateFile("line", line);
-    generateFile("segment", segment);
-    //    generateFile("ray", ray);
-    generateFile("parabolaSegment", parabolaSegment);
+    QVector<QPair<const Path*, int>> mv;
+    mv.reserve(segments.size());
+    for (const Path& p : segments)
+        mv.append(QPair{ &p, 0 });
+    bool rm = false;
+    do {
+        qDebug() << "mv 1" << mv.size();
+        for (int i = 0; i < mv.size(); ++i) {
+            progress(mv.size(), i); // progress
+            const IntPoint &p11 = mv[i].first->first(), &p12 = mv[i].first->last();
+            int& key1 = mv[i].second;
+            //            if (key1 == 3)
+            //                continue;
+            if (p11.X == minX - uScale || p11.X == maxX + uScale || p11.Y == minY - uScale || p11.Y == maxY + uScale)
+                key1 |= 1;
+            if (p12.X == minX - uScale || p12.X == maxX + uScale || p12.Y == minY - uScale || p12.Y == maxY + uScale)
+                key1 |= 2;
+            for (int j = 0; j < mv.size(); ++j) {
+                const IntPoint &p21 = mv[j].first->first(), &p22 = mv[j].first->last();
+                int& key2 = mv[j].second;
+                if (i == j /*|| key2 == 3*/)
+                    continue;
+                auto test = [](const IntPoint& p1, const IntPoint& p2) -> bool {
+                    return abs(p1.X - p2.X) < uScale * 0.005 && abs(p1.Y - p2.Y) < uScale * 0.005;
+                };
+                if (test(p11, p21)) {
+                    key1 |= 1;
+                    key2 |= 1;
+                } else if (test(p11, p22)) {
+                    key1 |= 1;
+                    key2 |= 2;
+                } else if (test(p12, p21)) {
+                    key1 |= 2;
+                    key2 |= 1;
+                } else if (test(p12, p22)) {
+                    key1 |= 2;
+                    key2 |= 2;
+                }
+            }
+        }
+        rm = false;
+        for (int i = 0; i < mv.size(); ++i) {
+            int& key = mv[i].second;
+            if (key != 3) {
+                rm = true;
+                mv.remove(i--);
+                continue;
+            }
+            key = 0;
+        }
+        qDebug() << "mv 2" << mv.size();
+    } while (rm);
+
+    Paths ppp;
+    ppp.reserve(mv.size());
+    for (int i = 0; i < mv.size(); ++i)
+        ppp.append(*mv[i].first);
+
+    generateFile("segments", /*segments*/ ppp);
+
+#endif
 
 #ifdef JCV
     QVector<jcv_point> points;
@@ -378,8 +314,8 @@ void VoronoiCreator::createVoronoi(const Tool& tool, double depth, const double 
     //progressOrCancel(7, 3); // progress
     {
         jcv_rect bounding_box = {
-            { static_cast<jcv_real>(r.left - uScale * 1.1), static_cast<jcv_real>(r.top - uScale * 1.1) },
-            { static_cast<jcv_real>(r.right + uScale * 1.1), static_cast<jcv_real>(r.bottom + uScale * 1.1) }
+            { static_cast<jcv_real>(r.left - uScale), static_cast<jcv_real>(r.top - uScale) },
+            { static_cast<jcv_real>(r.right + uScale), static_cast<jcv_real>(r.bottom + uScale) }
         };
         jcv_diagram diagram;
         jcv_diagram_generate(points.size(), points.data(), &bounding_box, &diagram);
@@ -392,7 +328,7 @@ void VoronoiCreator::createVoronoi(const Tool& tool, double depth, const double 
             jcv_graphedge* graph_edge = sites[i].edges;
             while (graph_edge) {
                 const jcv_edge* edge = graph_edge->edge;
-                const Pair pair { toIntPoint(edge, 0), toIntPoint(edge, 1), sites[i].p.id };
+                const Pair pair{ toIntPoint(edge, 0), toIntPoint(edge, 1), sites[i].p.id };
                 if (edge->sites[0] == nullptr || edge->sites[1] == nullptr)
                     frame.insert(pair); // frame
                 else if (edge->sites[0]->p.id != edge->sites[1]->p.id)
