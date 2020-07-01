88d26d5d429e013d41fa9c597cbb5040d158c237
X-Ray <xray3d@ya.ru>
2020-07-01 12:32:48 +0300

    PVS-Studio

--- G2G/forms/drillform/drillpreviewgi.cpp
+++ G2G/forms/drillform/drillpreviewgi.cpp
@@ -1,3 +1,7 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "drillpreviewgi.h"
 #include "tooldatabase/tool.h"
 #include <QPainter>
--- G2G/forms/thermal/thermalmodel.cpp
+++ G2G/forms/thermal/thermalmodel.cpp
@@ -1,3 +1,7 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "thermalmodel.h"
 
 QIcon ThermalModel::repaint(QColor color, const QIcon& icon) const
