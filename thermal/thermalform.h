38ddbcfaac86414eeae29355cb8d855d3f1f7fd3
X-Ray <xray3d@ya.ru>
2020-10-06 00:22:40 +0300

    5/10/20

--- thermal/thermalform.h
+++ thermal/thermalform.h
@@ -33,7 +33,8 @@ private slots:
     void on_cbxFileCurrentIndexChanged(int index);
     void on_dsbxDepth_valueChanged(double arg1);
 
-    void on_pbReset_clicked();
+    void on_dsbxAreaMax_editingFinished();
+    void on_dsbxAreaMin_editingFinished();
 
 private:
     Ui::ThermalForm* ui;
@@ -46,6 +47,8 @@ private:
     void setSelection(const QModelIndex& selected, const QModelIndex& deselected);
 
     ThParam par;
+    double lastMax;
+    double lastMin;
 
     QCheckBox* chbx;
     double m_depth;
