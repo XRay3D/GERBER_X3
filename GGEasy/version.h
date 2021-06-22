65b8e23e250a00d7b60234b3ac89c9f4ead93bd8
XRay3D <xray3d@ya.ru>
2021-06-22 14:49:20 +0300

    Ver 0.8.0.1

--- GGEasy/version.h
+++ GGEasy/version.h
@@ -13,8 +13,8 @@
 *******************************************************************************/
 #pragma once
 
-#define VER_FILEVERSION 0, 8, 0, 0
-#define VER_FILEVERSION_STR "0.8.0.0\0"
+#define VER_FILEVERSION 0, 8, 0, 1
+#define VER_FILEVERSION_STR "0.8.0.1\0"
 
 #define VER_PRODUCTVERSION VER_FILEVERSION
 #define VER_PRODUCTVERSION_STR VER_FILEVERSION_STR
