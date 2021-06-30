b091dfe9aaec9c096ec3deb58fb9fa6594309f5a
XRay3D <xray3d@ya.ru>
2021-06-30 13:38:20 +0300

    Style

--- GGEasy/style.cpp
+++ GGEasy/style.cpp
@@ -314,3 +314,106 @@ void Style::drawItemPixmap(QPainter* painter, const QRect& rect, int alignment, 
 {
     painter->drawPixmap(rect, pixmap, pixmap.rect());
 }
+
+MyStyle::MyStyle()
+    : QCommonStyle()
+{
+}
+
+void MyStyle::drawControl(QStyle::ControlElement element, const QStyleOption* opt, QPainter* p, const QWidget* w) const
+{
+    switch (element) {
+    case CE_ShapedFrame: {
+        QLinearGradient gradient(0, 0, 0, opt->rect.height());
+        gradient.setColorAt(0, QColor(255, 255, 255));
+        gradient.setColorAt(0.5, QColor(225, 225, 225));
+        gradient.setColorAt(1, QColor(255, 255, 255));
+        QPen pen;
+        pen.setBrush(gradient);
+        p->setPen(pen);
+        p->drawRect(0, 0, opt->rect.width() - 1, opt->rect.height() - 1);
+    } break;
+    case CE_ItemViewItem: {
+        drawPrimitive(PE_PanelItemViewItem, opt, p, w);
+        auto option = qstyleoption_cast<const QStyleOptionViewItemV4*>(opt);
+        if (!option) {
+            QCommonStyle::drawControl(element, opt, p, w);
+            return;
+        }
+
+        if (option->state & QStyle::State_HasFocus) {
+            QPen pen(QColor(170, 170, 170));
+            p->save();
+            p->setPen(pen);
+            p->drawRect(opt->rect.x(), opt->rect.y(), opt->rect.width() - 1, opt->rect.height() - 1);
+            p->restore();
+        }
+
+        QRect textRect = subElementRect(SE_ItemViewItemText, option, w);
+        textRect.setX(textRect.x() + 5);
+        textRect.setY(textRect.y() + 5);
+        if (!option->text.isEmpty()) {
+            p->drawText(textRect, option->text);
+        }
+    } break;
+    case CE_Header: {
+        QCommonStyle::drawControl(element, opt, p, w);
+    } break;
+    case CE_HeaderEmptyArea: {
+        p->fillRect(opt->rect, QBrush(QColor(233, 233, 233)));
+    } break;
+    case CE_HeaderSection: {
+        if (opt->state & State_Sunken || opt->state & State_On) {
+            p->fillRect(opt->rect, QBrush(QColor(255, 170, 80)));
+            p->save();
+            p->setPen(QPen(QColor(170, 170, 170)));
+            p->drawRect(opt->rect.x(), opt->rect.y(), opt->rect.width() - 1, opt->rect.height() - 1);
+            p->restore();
+        } else {
+            QLinearGradient gradient(0, 0, 0, opt->rect.height());
+            gradient.setSpread(QGradient::PadSpread);
+            gradient.setColorAt(0, QColor(255, 255, 255));
+            gradient.setColorAt(1, QColor(220, 220, 220));
+            p->fillRect(opt->rect, QBrush(gradient));
+
+            gradient.setColorAt(0, QColor(230, 230, 230));
+            gradient.setColorAt(0.5, QColor(175, 175, 175));
+            gradient.setColorAt(1, QColor(230, 230, 230));
+
+            QPen pen;
+            pen.setStyle(Qt::SolidLine);
+            pen.setBrush(gradient);
+            p->setPen(pen);
+            p->drawLine(opt->rect.width() + opt->rect.x() - 1,
+                opt->rect.y() + 3,
+                opt->rect.width() + opt->rect.x() - 1,
+                opt->rect.height() + opt->rect.y() - 3);
+        }
+    } break;
+    default: {
+        QCommonStyle::drawControl(element, opt, p, w);
+    } break;
+    }
+}
+
+void MyStyle::drawItemText(QPainter* painter, const QRect& rect, int flags, const QPalette& pal, bool enabled, const QString& text, QPalette::ColorRole textRole) const
+{
+    painter->setPen(QPen(QColor(30, 30, 30)));
+    painter->setFont(QFont("Consolas"));
+    painter->drawText(rect, text);
+}
+
+void MyStyle::drawPrimitive(QStyle::PrimitiveElement pe, const QStyleOption* opt, QPainter* p, const QWidget* w) const
+{
+    switch (pe) {
+    case PE_PanelItemViewItem: {
+        if (auto option = qstyleoption_cast<const QStyleOptionViewItemV4*>(opt))
+            if ((option->state & QStyle::State_Selected))
+                p->fillRect(option->rect, QBrush(QColor(220, 220, 220, 100)));
+
+    } break;
+    default: {
+        QCommonStyle::drawPrimitive(pe, opt, p, w);
+    } break;
+    }
+}
