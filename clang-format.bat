46e7e8e7cd012bdf7134a26d26e521a9069da4d4
XRay3D <xray3d@ya.ru>
2022-08-28 01:10:33 +0300

    clang-format

--- clang-format.bat
+++ clang-format.bat
@@ -1,26 +1,10 @@
-
-REM set Ms=x15 55 xx4*8 112.6 12 800 88.1 sss
-REM set Ss1=xx4*8 112.6 12 800 88.1
-
-REM call set Replaced=%%Ms:%Ss1%=%%
-
-REM echo Ms:       %Ms%
-REM echo Ss1:      %Ss1%
-REM echo Replaced: %Replaced%
-
-REM If NOT "%Ms%"=="%Replaced%" (
-     REM echo Ms contains Ss1
-REM ) else (
-     REM echo Ms does not contain Ss1
-REM )
-
-REM pause
-
-rem set PATH="%PATH%C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin"
-
 set CLANG_FORMAT="C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\x64\bin\clang-format.exe"
 
-for /R %%S in (*.cpp *.c *.hpp *.h) do (
-	 %CLANG_FORMAT% -i --style="{BasedOnStyle: WebKit,AllowShortLambdasOnASingleLine: All,BreakBeforeTernaryOperators: 'false',Standard: Cpp11,BreakBeforeBraces: Custom,FixNamespaceComments: 'true',AlignConsecutiveMacros: 'true',AlignTrailingComments: 'true',Cpp11BracedListStyle: 'true',AlignArrayOfStructures: Right}" %%S
+for %%P in (GGEasy GTE_Win plugins static_libs) do (
+	cd %%P 
+	for /R %%S in (*.cpp *.c *.hpp *.h) do (
+	    %CLANG_FORMAT% -i --style="{BasedOnStyle: WebKit,AllowShortLambdasOnASingleLine: All,BreakBeforeTernaryOperators: 'false',Standard: Cpp11,BreakBeforeBraces: Custom,FixNamespaceComments: 'true',AlignConsecutiveMacros: 'true',AlignTrailingComments: 'true',Cpp11BracedListStyle: 'true',AlignArrayOfStructures: Right}" %%S
+	)
+	cd ..
 )
 pause
