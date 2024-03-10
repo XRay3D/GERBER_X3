27c5e16e64740bcb7d2831a0a416957768509481
Damir Bakiev (X-Ray) <xray3d@ya.ru>
2024-03-10 12:52:43 +0300

    Поправил поведение редактирования окружности.

--- plugins/file/gerber/gbr_highlighter.cpp
+++ plugins/file/gerber/gbr_highlighter.cpp
@@ -29,20 +29,23 @@ void SyntaxHighlighter::highlightBlock(const QString& text) {
         return;
     }
 
-    static const std::map<QChar, QColor> color{
-        {'D', QColor(0x00, 0xFF, 0xFF)},
-        {'G', QColor(0x7F, 0x7F, 0x7F)},
-        {'I', QColor(0x00, 0x00, 0xFF)},
-        {'J', QColor(0xFF, 0x00, 0xFF)},
-        {'M', QColor(0x00, 0xFF, 0xFF)},
-        {'X', QColor(0xFF, 0x00, 0x00)},
-        {'Y', QColor(0x00, 0xFF, 0x00)},
+    static constexpr auto color = [](QChar c) -> QColor {
+        switch(c.toLatin1()) {
+        case 'D': return {0x00, 0xFF, 0xFF};
+        case 'G': return {0x7F, 0x7F, 0x7F};
+        case 'I': return {0x00, 0x00, 0xFF};
+        case 'J': return {0xFF, 0x00, 0xFF};
+        case 'M': return {0x00, 0xFF, 0xFF};
+        case 'X': return {0xFF, 0x00, 0x00};
+        case 'Y': return {0x00, 0xFF, 0x00};
+        default: return {};
+        }
     };
 
     std::u16string_view data(reinterpret_cast<const char16_t*>(text.utf16()), text.size());
 
-    for(auto&& m: ctre::range<R"([DGIJMXY][+\-]?\d+\.?\d*)">(data)) {
-        myClassFormat.setForeground(color.at(*m.data()));
+    for(auto&& m: ctre::search_all<R"([DGIJMXY][+\-]?\d+\.?\d*)">(data)) {
+        myClassFormat.setForeground(color(*m.data()));
         int start = std::distance(data.data(), m.data());
         int count = static_cast<int>(m.size());
         setFormat(start, count, myClassFormat);
