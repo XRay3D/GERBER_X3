680f9631478b7a533a85e8d8aa1b74b9531b14a1
Bakiev Damir <xray3d@ya.ru>
2026-04-13 11:43:33 +0300

    Fix Drill::create & save stacktrace.

--- static_libs/gi/gi_drill.cpp
+++ static_libs/gi/gi_drill.cpp
@@ -65,20 +65,20 @@ void Drill::updatePath(const QPolygonF& path, double diameter) {
     update();
 }
 
-Paths Drill::paths(int /*alternate*/) const {
-    auto path{shape_.toFillPolygon(QTransform::fromScale(100, 100))};
-    path = QTransform::fromScale(0.01, 0.01).map(path);
-    return {~transform().map(path)};
-}
+// Paths Drill::paths(int /*alternate*/) const {
+//     auto path{shape_.toFillPolygon(QTransform::fromScale(100, 100))};
+//     path = QTransform::fromScale(0.01, 0.01).map(path);
+//     return {~transform().map(path)};
+// }
 
 void Drill::changeColor() {
     // animation.setStartValue(bodyColor_);
 
     switch(colorState) {
-    case Default           : brushColor_ = QColor(100, 100, 100); break;
-    case Hovered           : brushColor_ = QColor(150, 0x0, 150); break;
-    case Selected          : brushColor_ = QColor(255, 0x0, 255); break;
-    case Hovered | Selected: brushColor_ = QColor(127, 0x0, 255); break;
+    case Default           : brushColor_ = QColor{100, 100, 100}; break;
+    case Hovered           : brushColor_ = QColor{150, 150, 150}; break;
+    case Selected          : brushColor_ = QColor{255, 0x0, 255}; break;
+    case Hovered | Selected: brushColor_ = QColor{255, 150, 255}; break;
     }
 
     penColor_ = brushColor_;
@@ -94,28 +94,21 @@ void Drill::changeColor() {
 }
 
 void Drill::create() {
-    shape_ = QPainterPath{};
+    if(path_.empty()) return;
 
-    if(!path_.size()) {
-        return;
-    } else if(path_.size() == 1) {
-        // path_ = CirclePath(double(diameter_ ? diameter_ * uScale : uScale), path_.front());
-        // ReversePath(path_);
-        // path_.push_back(path_.front());
-        // shape_.addPolygon(path_);
+    shape_.clear();
+    curves_.clear();
+
+    if(path_.size() == 1) {
         shape_.addEllipse(path_.front(), diameter_ * 0.5, diameter_ * 0.5);
-        // path_ = shape_.toFillPolygon();
+        curves_.emplace_back(CircleCurve(diameter_, path_.front()));
     } else {
-        qFatal("toCurve");
-        boundingRect_ = shape_.boundingRect();
-        for(auto&& path: Inflate(Paths{~path_}, diameter_ * uScale, JoinType::Round, EndType::Round, uScale)) {
-            path.push_back(path.front());
-            shape_.addPolygon(~path);
-        }
+        Path path = ~path_;
+        r::for_each(path, SetCSelf);
+        curves_ = toCurves(InflateRoundPolygon({path}, diameter_ * uScale));
+        shape_ = toPPath(curves_);
     }
-
     boundingRect_ = shape_.boundingRect();
-    fillPolygon = shape_.toFillPolygon();
 }
 
 } // namespace Gi
